05abd1b519ae43a9fb3120ea6afb95e4333c3e0e
Vitor Miranda <vitor.alves.miranda@hotmail.com>
2025-05-15 00:22:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-const modal = document.querySelector(".modal")
-const mascara = document.querySelector(".mascara-modal")
+const modal = document.querySelector(".modal");
+const mascara = document.querySelector(".mascara-modal");
+const modalAvaliacoes = document.querySelector(".modal-avaliacoes");
+const mascaraAvaliacoes = document.querySelector(".mascara-avaliacoes");
 
 function mostrarModal(){
-   modal.style.left = "50%"
-   mascara.style.visibility = "visible"
+    modal.style.left = "50%";
+    mascara.style.visibility = "visible";
 }
 
 function esconderModal() {
-    modal.style.left = "-50%"
-   mascara.style.visibility = "hidden"
+    modal.style.left = "-50%";
+    mascara.style.visibility = "hidden";
 }
 
+function mostrarAvaliacoes(){
+    modalAvaliacoes.classList.add("visible");
+    mascaraAvaliacoes.classList.add("visible");
+}
+
+function esconderAvaliacoes() {
+    modalAvaliacoes.classList.remove("visible");
+    mascaraAvaliacoes.classList.remove("visible");
+}
+
+
+function filtrarBolos(categoria) {
+  const bolos = document.querySelectorAll('.bolo-item');
+  bolos.forEach(bolo => {
+    const cat = bolo.getAttribute('data-categoria');
+    if (categoria === 'todos' || cat === categoria) {
+      bolo.style.display = 'block';
+    } else {
+      bolo.style.display = 'none';
+    }
+  });
+}
